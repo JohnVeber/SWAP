3da56d9c3556394dacc34099dbf18d306ac41013
John Veber <evgeniy.weber@gmail.com>
2024-04-30 18:16:00 +0300

    v.1.0.1 added.

--- swap.c
+++ swap.c
@@ -1,28 +1,27 @@
 /* swap.c*/
-/* Version v0.0.1*/
-/* This program is the first attempt to realise swap function in C without pointers usage */
+
+/* This program is the second attempt to realise swap function in C */
 /* The function works for integers only at this version of program */
+/* To achieve the goal of this programm pointers are used */
 
 #include <stdio.h>
 
-void swap_i (int, int); // Function declaration
+void swap_i (int *pc, int *pd); // Function declaration
 
 int main (void)
 {
     int a = 21;
     int b = 17;
-    printf("Before swapping: a = %d, b = %d\n", a, b);
-    swap_i(a, b); // Function call
-    printf("After swapping: a = %d, b = %d\n\n\n", a, b);
+    printf("Values before swapping were: a = %d, b = %d.\n", a, b);
+    swap_i(&a, &b); // Function call
+    printf("Values after swapping are: ");
+    printf("a = %d, b = %d\n\n\n", a, b);
     return 0;
 }
-void swap_i(int c, int d) // Function definition
+void swap_i(int *pc, int *pd) // Function definition
 {
-    int t = c;
-    c = d;
-    d = t;
-    printf("Inside function swap_i: a = %d, b = %d\n", c, d);
+    int t = *pc;
+    *pc = *pd;
+    *pd = t;
+    return;
 }
-/* The program output demonstrates passing arguments by value: 
-variables at the "main" function were not swapped.*/
-/* Target of the program wasn't achieved.*/
