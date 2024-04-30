2a54d5a7920eb1637b40fb42fdba74fdb14a5c44
John Veber <evgeniy.weber@gmail.com>
2024-04-30 22:32:43 +0300

    v.2.0.2 added. First test added to the project.

---
+++ test/test_1.c
@@ -0,0 +1,18 @@
+// test_1.c 
+// With this test, two values are passed into a function\
+ and output data compared with expected.
+
+#include <stdio.h>
+#include <assert.h>
+#include "../src/swap_func.h"
+void pair(void){
+    int f = 10;
+    int s = 20;
+    swap_i(&f, &s);
+    assert(f == 20 && s == 10);
+    printf("Test 1 passed.\n");
+}
+
+int main(void){
+    pair();
+}
