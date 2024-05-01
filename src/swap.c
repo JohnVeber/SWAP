969a84d3e0a0cabd0fcfb1d3389c2164a0068cb3
John Veber <evgeniy.weber@gmail.com>
2024-05-01 12:15:53 +0300

    Added v.3.0.3. Bug fixed.

--- src/swap.c
+++ src/swap.c
@@ -1,6 +1,6 @@
 /* swap.c*/
 
-// Current version v.3.0.2
+// Current version v.3.0.3
 
 /* Task: Use command line arguments to enter swapped values*/
 
@@ -8,13 +8,13 @@
 #include <stdlib.h> // For atoi() usage
 #include "swap_func.h"
 
-int main (int argc, char *argv[])
-{
+
+int main (int argc, char *argv[]){
     int a, b;
-    a = atoi(argv[1]); // Use atoi to cast numbers in string form to their integer values.
-    b = atoi(argv[2]); 
 
     if (argc == 3){
+    a = atoi(argv[1]); // Use atoi to cast numbers in string form to their integer values.
+    b = atoi(argv[2]); 
         printf("You entered %d and %d.\n", a, b);
         swap_i(&a, &b); // Function call
 
@@ -32,5 +32,6 @@ int main (int argc, char *argv[])
 
 // Bugs reported:
 //If only one number entered (argc == 2) segfault is observed.
+//Bug fixed: Variable assignments have been moved inside "if" condition.
 
 // Problems found: 
