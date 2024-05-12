c654f7acb6b811a9cb80db437e2f067984df4918
John Veber <evgeniy.weber@gmail.com>
2024-05-12 20:34:17 +0300

    Test commit. No significant changes.

---
+++ build/test/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/john_ubuntu_1/PROJECTS/SWAP/src/quantity_func.c" "test/CMakeFiles/tests.dir/__/src/quantity_func.c.o" "gcc" "test/CMakeFiles/tests.dir/__/src/quantity_func.c.o.d"
+  "/home/john_ubuntu_1/PROJECTS/SWAP/src/swap_func.c" "test/CMakeFiles/tests.dir/__/src/swap_func.c.o" "gcc" "test/CMakeFiles/tests.dir/__/src/swap_func.c.o.d"
+  "/home/john_ubuntu_1/PROJECTS/SWAP/test/tests.c" "test/CMakeFiles/tests.dir/tests.c.o" "gcc" "test/CMakeFiles/tests.dir/tests.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/__/src/quantity_func.c.o"
+  "CMakeFiles/tests.dir/__/src/quantity_func.c.o.d"
+  "CMakeFiles/tests.dir/__/src/swap_func.c.o"
+  "CMakeFiles/tests.dir/__/src/swap_func.c.o.d"
+  "CMakeFiles/tests.dir/tests.c.o"
+  "CMakeFiles/tests.dir/tests.c.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
